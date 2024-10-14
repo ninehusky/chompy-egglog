1e70596e993051c083f5f8b71ff3745984f8282d
Andrew Cheung <a7cheung@ucsd.edu>
2024-10-14 10:10:55 -0700

    More tinkering

--- tests/math.rs
+++ tests/math.rs
@@ -1,4 +1,5 @@
 use egglog::EGraph;
+use ruler::enumo;
 
 #[test]
 fn math_eval() {
@@ -9,6 +10,33 @@ fn math_eval() {
             include_str!("egglog/math-old.egg"),
         )
         .unwrap();
+
+    // begin by enumerating the language
+
+    let unary_fns = enumo::Workload::new(&["(Abs)"]);
+    let binary_fns = enumo::Workload::new(&["(Add)", "(Sub)", "(Mul)", "(Div)"]);
+
+    let pred_binary_fns = enumo::Workload::new(&["(Eq)", "(Neq)"]);
+
+    let atoms = enumo::Workload::new(&["(Num 0)", "(Num 1)", "(Var quoteaquote)"]);
+
+    let term_productions = enumo::Workload::new(&["(MathOp2 mathop x y)", "(PredOp2 predop x y)"])
+        .plug("mathop", &binary_fns)
+        .plug("predop", &pred_binary_fns);
+
+    let terms_depth_1 = term_productions.plug("x", &atoms).plug("y", &atoms);
+
+    let mut prog: String = String::new();
+    for term in terms_depth_1.force() {
+        let term_str = term.to_string().replace("quote", "\"");
+        prog = format!("{}\n{}", prog, term_str);
+    }
+    prog += "\n(run 20)\n";
+
+    let extracts = egraph.parse_and_run_program(None, &prog).unwrap();
+    for extract in extracts {
+        println!("{}", extract);
+    }
     let serialized = egraph.serialize(egglog::SerializeConfig::default());
     serialized.to_json_file("math.svg").unwrap();
 }
