ee9437a69c3f076d0a8195a9d247fd9472524869
Andrew Cheung <a7cheung@ucsd.edu>
2025-01-17 00:51:50 -0800

    Update

--- tests/math/mod.rs
+++ tests/math/mod.rs
@@ -16,37 +16,37 @@ pub mod tests {
         super::evaluate_ruleset, MiddleSchoolLangChomper, MiddleSchoolLangConditionsChomper,
     };
 
-    // #[test]
-    // fn check_math_rules() {
-    //     let chomper = MathChomper;
-    //     let predicates = chomper.get_language().get_predicates();
-    //     let values = chomper.get_language().get_vals();
-    //     for v in values {
-    //         println!("Value: {}", chomper.get_language().make_val(v));
-    //     }
-    //     for p in predicates.force() {
-    //         println!("Predicate: {}", p);
-    //     }
-    //     let rules = chomper.run_chompy(5);
-    //     let hand_picked_rules = vec![
-    //         Rule {
-    //             condition: Sexp::from_str("(Neq ?x (Const 0))").ok(),
-    //             lhs: Sexp::from_str("(Div ?x ?x)").unwrap(),
-    //             rhs: Sexp::from_str("(Const 1)").unwrap(),
-    //         },
-    //         Rule {
-    //             condition: Sexp::from_str("(Gt ?x (Const -1))").ok(),
-    //             lhs: Sexp::from_str("(Abs ?x)").unwrap(),
-    //             rhs: Sexp::from_str("?x").unwrap(),
-    //         },
-    //     ];
-    //     evaluate_ruleset::<MathChomper, MathLang>(
-    //         &rules,
-    //         &hand_picked_rules,
-    //         chomper,
-    //         MathLang::Var("dummy".into()),
-    //     );
-    // }
+    #[test]
+    fn check_math_rules() {
+        let chomper = MathChomper;
+        let predicates = chomper.get_language().get_predicates();
+        let values = chomper.get_language().get_vals();
+        for v in values {
+            println!("Value: {}", chomper.get_language().make_val(v));
+        }
+        for p in predicates.force() {
+            println!("Predicate: {}", p);
+        }
+        let rules = chomper.run_chompy(7);
+        let hand_picked_rules = vec![
+            Rule {
+                condition: Sexp::from_str("(Neq ?x (Const 0))").ok(),
+                lhs: Sexp::from_str("(Div ?x ?x)").unwrap(),
+                rhs: Sexp::from_str("(Const 1)").unwrap(),
+            },
+            Rule {
+                condition: Sexp::from_str("(Gt ?x (Const -1))").ok(),
+                lhs: Sexp::from_str("(Abs ?x)").unwrap(),
+                rhs: Sexp::from_str("?x").unwrap(),
+            },
+        ];
+        evaluate_ruleset::<MathChomper, MathLang>(
+            &rules,
+            &hand_picked_rules,
+            chomper,
+            MathLang::Var("dummy".into()),
+        );
+    }
 
     // #[test]
     // fn middle_school_total_tests() {
@@ -56,13 +56,13 @@ pub mod tests {
     //     chomper.run_chompy(6);
     // }
 
-    #[test]
-    fn middle_school_conditional_tests() {
-        let chomper = MiddleSchoolLangConditionsChomper {
-            math_chomper: MathChomper,
-        };
-        chomper.run_chompy(6);
-    }
+    // #[test]
+    // fn middle_school_conditional_tests() {
+    //     let chomper = MiddleSchoolLangConditionsChomper {
+    //         math_chomper: MathChomper,
+    //     };
+    //     chomper.run_chompy(8);
+    // }
 }
 
 pub struct MiddleSchoolLang {
