f52e2b4d770f3b46d595ef241a59b94c17ad2b37
Andrew Cheung <a7cheung@ucsd.edu>
2025-01-30 20:55:58 -0800

    Get rid of predicate interpreter

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,2 @@
 pub mod chomper;
 pub mod language;
-
-use ruler::enumo::Sexp;
-use std::fmt::Debug;
-
-/// Helper trait which helps plug in an arbitrary interpreter for the conditional rule.
-pub trait PredicateInterpreter: Debug + Send + Sync {
-    fn interp_cond(&self, sexp: &Sexp) -> bool;
-}
